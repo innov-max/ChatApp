ef3b01c84fe76bed7261fc1f9e6335c0e3d1a7b1
innov-max <maxwell.opondo@strathmore.edu>
2023-05-09 23:29:20 +0100

    Initial commit

---
+++ app/src/main/java/com/innov/chatapp/Activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.innov.chatapp.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.innov.chatapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
