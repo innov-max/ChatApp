40c8fd0c707fa12d61fd0bca232dccf491ffb623
innov-max <maxwell.opondo@strathmore.edu>
2023-05-10 21:56:32 +0100

    otpActivityImplementation

--- app/src/main/java/com/innov/chatapp/Activity/Verification.kt
+++ app/src/main/java/com/innov/chatapp/Activity/Verification.kt
@@ -1,11 +1,35 @@
 package com.innov.chatapp.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.innov.chatapp.R
+import com.innov.chatapp.databinding.ActivityMainBinding
+import com.innov.chatapp.databinding.ActivityVerificationBinding
 
 class Verification : AppCompatActivity() {
+
+    var binding :ActivityVerificationBinding? = null
+    var auth : FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_verification)
+        binding = ActivityVerificationBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        auth = FirebaseAuth.getInstance()
+        if (auth!!.currentUser != null){
+            val intent = Intent(this@Verification,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        supportActionBar!!.hide()
+        binding!!.editNumber.requestFocus()
+        binding!!.continueBtn.setOnClickListener {
+            val intent = Intent(this@Verification,OTPActivity::class.java)
+            intent.putExtra("phoneNumber",binding!!.editNumber.text.toString())
+            startActivity(intent)
+
+        }
     }
 }
---
+++ app/src/main/java/com/innov/chatapp/Activity/setupProfile.kt
@@ -0,0 +1,15 @@
+package com.innov.chatapp.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.innov.chatapp.R
+import com.innov.chatapp.databinding.ActivityOtpactivityBinding
+
+class setupProfile : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setup_profile)
+    }
+}
